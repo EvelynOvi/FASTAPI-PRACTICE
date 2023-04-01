ccdcf9e21351ea47df979e7d7aa669b1f65944ea
EvelynOvi <oviangbedeevelyn@gmail.com>
2023-04-01 08:50:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+from fastapi import FastAPI
+from models import User
+
+app = FastAPI()
+
+db: List[User] = [
+    
+]
+@app.get("/")
+async def root():
+    return {"Hello": "Mundo"}
